c81a84fcbc25a0f4e423e8c9122285deb2bb8924
CarlosZoft <carlosk2gunn@hotmail.com>
2022-12-03 16:57:34 -0300

    ✨ feat : getting graph follow
    Co-authored-by: Karla Feliciano <78981063+KarlaCSF@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const { Octokit } = require("@octokit/core");
-const axios = require("axios");
+require("dotenv/config");
 const fs = require("fs");
 const _ = require("lodash");
 const contribuitors = require("./uniqueContributors.json");
 
+console.log(process.env.TOKEN);
 const octokit = new Octokit({
-  auth: process.env.TOKEN
+  auth: process.env.TOKEN,
 });
 
 const main = async () => {
-  // const usersFollowersPromisse = usersInfo.map(async (user) => {
-  //   const { data : followers } = await octokit.request('GET /users/{username}/followers{?per_page,page}', {
-  //     username: user.login
-  //   })
-  //   console.log(followers)
-  //   return {
-  //     userInfo: user,
-  //     followers
-  //   }
-  // })
+  const hashMap = {};
 
-  // const usersFollowers = await Promise.all(usersFollowersPromisse)
+  for (let i = 0; i < contribuitors.length; i++) {
+    hashMap[contribuitors[i].login] = [];
+  }
 
-  // console.log(usersFollowers)
+  const usersFollowersPromisse = contribuitors.map(
+    async (contributor) => {
+      const { login } = contributor;
+
+      let page = 1;
+      while (true) {
+        try {
+          const { data: followers } = await octokit.request(
+            "GET /users/{username}/followers",
+            {
+              username: login,
+              per_page: 100,
+              page,
+            }
+          );
+          console.log(`Fetching followers from ${login} page ${page}`);
+          const followersLogin = followers
+            .filter((follower) => hashMap[follower.login])
+            .map((follower) => follower.login);
+          hashMap[contributor.login].push(...followersLogin);
+          if (followers.length !== 100) break;
+        } catch (error) {
+          console.log(error.message);
+        }
+
+        page++;
+      }
+    }
+  );
+
+  // chunks of 415 promisses
+
+  await Promise.all(usersFollowersPromisse);
+
+  fs.writeFile("listOfFollowers.json", JSON.stringify(hashMap), (err) => {});
 };
 
 main();
