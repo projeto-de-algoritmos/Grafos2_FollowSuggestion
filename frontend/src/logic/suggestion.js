40fd77906c7589139590642a7d03141a1b134f56
CarlosZoft <carlosk2gunn@hotmail.com>
2022-12-05 23:40:47 -0300

    ♻️ refactor: improving code

--- frontend/src/logic/suggestion.js
+++ frontend/src/logic/suggestion.js
@@ -1,17 +1,20 @@
-import { Graph, inverseGraph } from './graph';
+import { Graph, inverseGraph } from "./graph";
 const SCC = Graph.getSCC();
 
 const suggestion = (login) => {
+  // Encontra o component do usuário
   const scc = SCC.find((component) => component.includes(login)) ?? [];
-  const suggestion = scc.filter((componentLogin) => componentLogin !== login);
 
-  const noFollowYet = suggestion.filter((componentLogin) => {
-    if(componentLogin === login) return false;
-    const isFollowed = Graph.getNeighbors(inverseGraph, login).includes(componentLogin);
+  // Encontra os usuários que não são seguidos pelo usuário solicitado
+  const noFollowYet = scc.filter((componentLogin) => {
+    if (componentLogin === login) return false;
+    const isFollowed = Graph.getNeighbors(inverseGraph, login).includes(
+      componentLogin
+    );
     return !isFollowed;
   });
-  
+
   return noFollowYet;
-}
+};
 
 export default suggestion;
